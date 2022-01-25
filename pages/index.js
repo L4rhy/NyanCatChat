f99df039307b08b48f2382871e7b6650824745d3
L4rhy <lara.vsteinbach@gmail.com>
2022-01-25 15:25:23 -0300

    comeco6

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,6 @@ export default function PaginaInicial() {
   const roteamento = useRouter();
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
