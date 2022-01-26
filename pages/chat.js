ff9a18fd78727ae5a9be695f087086b010d485e4
L4rhy <lara.vsteinbach@gmail.com>
2022-01-26 12:11:28 -0300

    aula3-2

--- pages/chat.js
+++ pages/chat.js
@@ -44,7 +44,7 @@ export default function ChatPage() {
                     }}
                 >
 
-                    {/* <MessageList mensagens={[]} /> */}
+                    {<MessageList mensagens={[]} />}
 
                     <Box
                         as="form"
